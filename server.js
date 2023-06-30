6d7bd0d981c986dcceef8ac48758cf6ed3b9f405
Yasir <yasirnawab444@gmail.com>
2023-06-29 22:36:07 -0700

    error fixed

--- server.js
+++ server.js
@@ -22,11 +22,10 @@ dotenv.config();
 connectDb();
 
 //static files
-app.use(express.static(join.path(__dirname, "./client/build")));
-
-app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname, "./client/build/index.html"))
-})
+app.use(express.static(path.join(__dirname,'./client/build')));
+app.get("*", (req,res)=>{
+    res.sendFile(path.join(__dirname,"./client/build/index.html"));
+});
 //middlewares
 // app.use(cors());
 // app.use(morgan('dev'));
